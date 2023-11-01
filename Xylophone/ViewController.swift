fe7da7ed1d97e49ad08bfa84719aa3c6feaf2e1f
davidsantander9@gmail.com <davidsantander9@gmail.com>
2023-10-31 18:37:51 -0600

    play a sound

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,14 +7,36 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var audioPlayer = AVAudioPlayer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+    }
+    
+    func playSound(note sound: String){
+        guard let sound = Bundle.main.path(forResource: "C", ofType: "wav") else {
+            print("Error getting the wav file from the main bundle.")
+            return
+        }
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default )
+            audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: sound))
+        } catch {
+            print("Audio file error.")
+        }
+        audioPlayer.play()
     }
 
 
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound(note: "C")
+    }
     
 
 }
